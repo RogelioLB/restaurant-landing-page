6f6431f953bb88a43d0aac8e50b0eb415d445e35
= <rogelio20052011@gmail.com>
2025-06-20 15:24:43 -0700

    Tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import tailwind from '@astrojs/tailwind';
 
 import react from "@astrojs/react";
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
